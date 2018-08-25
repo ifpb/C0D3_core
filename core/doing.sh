c68dda064a36ea87188deb9fa93e8d1a0257750e
Michael Almeida da Franca Monteiro <michael.cstrc@gmail.com>
2018-08-25 18:29:25 -0300

    [feat] Atualizado o formato do Job de entrada
    Foram feitas alterações no formato de entrada do job para permitir
    uma maior flexibilidade na criação de jobs.
    
    O script doing.sh foi alterado para refletir às modificações no
    formato do job.

--- core/doing.sh
+++ core/doing.sh
@@ -1,63 +1,53 @@
 #/bin/bash
 
 # Compiles and executes the problem code
+function compile()
+{
+	path=$1
+	cd DOING/$path
+	mkdir result
+	gcc code.c &> result/compile.out
+	echo $? > result/compile.return
+	cd ../..
+}
+
+function differ()
+{
+	arquivo=$1
+	path=$2
+	ext=`echo ${arquivo} | cut -d. -f2`
+	diff DOING/$path/out.$ext DOING/$path/result/stdout.$ext > DOING/$path/result/diff.$ext
+}
+
 function execut()
 {
 	arquivo=$1
+	path=$2
 	ext=`echo ${arquivo} | cut -d. -f2`
-	if [ "${comando}" = "c" ]
-	then
-		gcc input.$ext
-		cat $arquivo | a.out > a.$ext 
-		diff out.$ext a.$ext > diff.$ext
-		echo "$?\n" >> out.lst
-	elif [ "${comando}" = "py" ]
-	then
-		python3 ... -o a.$ext
-		diff out.$ext a.$ext > /dev/null
-		echo "$?\n" >> out.lst
-	elif [ "${comando}" = "java" ]
-	then
-		java ... -o a.$ext
-		diff out.$ext a.$ext > /dev/null
-		echo "$?\n" >> out.lst
-	elif [ "${comando}" = "cpp" ]
-	then
-		g++ input.$ext -o a.$ext
-		diff out.$ext a.$ext > /dev/null
-		echo "$?\n" >> out.lst
-	fi
+	cd DOING/$path
+	echo ${arquivo} > a.out > result/stdout.$ext
+	echo ${arquivo} > a.out 2> result/stderr.$ext
+	cd ../..
 }
 
 # Main loop: look for new jobs every 1 sec
 while :; do
 
-	sleep 1
+	sleep 5
 
 	# if there are new jobs
-	if [ `ls JOBS/ | wc -l` > 0 ]
+	if [ `ls JOBS/ | grep -E ^Job. | wc -l` -gt 0 ]
 	then
 		path=`ls JOBS/ | head -1`
-		ext=`echo $path | cut -d. -f2`
-		mv -r JOBS/$path doing/
-		for i in $(ls in.*);
+		mv JOBS/$path DOING/
+		compile $path
+		for i in `ls DOING/$path/ | grep -E ^in.*`;
 		do
-			execut $i
+			execut $i $path
+			differ $i $path
 		done
-		number=`cat out.lst | wc -l`
-		count=0
-		for ((i=0; i<${number}; i++))
-		do
-			if [ `cat out.lst | head -$i | tail -1` = 1 ]
-			then
-				count++
-			fi
-		done
-		if [ ${count} = ${number} ]
-		then
-			echo "Beleza!"
-		else
-			echo "Deu merda!"
-		fi
+		mv DOING/$path DONE/
+	else
+		echo "Nothing to do..."
 	fi
 done
