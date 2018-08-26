d9a82880942d2bbeeda990f15c7cd339b74e497f
Thiago <thiago.gouveia.da.silva@gmail.com>
2018-08-26 07:51:19 -0300

    [feat] Debug messages added to debug.sh
    Also, corrected some bugs on debug.sh:
    
    * ./a.out instead of a.out
    * cat input_file | ./a.out instead of echo input_file > a.out

--- core/doing.sh
+++ core/doing.sh
@@ -1,14 +1,25 @@
 #/bin/bash
 
+# Global Options
+DEBUG=1
+
 # Compiles and executes the problem code
 function compile()
 {
 	path=$1
+    old_dir=$(pwd)
 	cd DOING/$path
+    
+    # find the code_file on the folder
+    code_to_be_judged=( $(ls | grep *.c) )
+    code_to_be_judged=${code_to_be_judged[0]}
+    
+    [ $DEBUG -eq 1 ] && echo "code to be judged=${code_to_be_judged}"
+    
 	mkdir result
-	gcc code.c &> result/compile.out
+	gcc ${code_to_be_judged} &> result/compile.out
 	echo $? > result/compile.return
-	cd ../..
+	cd ${old_dir}
 }
 
 function differ()
@@ -16,29 +27,48 @@ function differ()
 	arquivo=$1
 	path=$2
 	ext=`echo ${arquivo} | cut -d. -f2`
-	diff DOING/$path/out.$ext DOING/$path/result/stdout.$ext > DOING/$path/result/diff.$ext
+    
+    [ $DEBUG -eq 1 ] && echo "Diff phase between: ${path}/out.${ext} and ${path}/result/stdout.${ext}"
+    
+	diff DOING/${path}/out.${ext} DOING/${path}/result/stdout.${ext} > DOING/${path}/result/diff.${ext}
 }
 
 function execut()
 {
 	arquivo=$1
 	path=$2
+    old_dir=$(pwd)
+        
 	ext=`echo ${arquivo} | cut -d. -f2`
 	cd DOING/$path
-	echo ${arquivo} > a.out > result/stdout.$ext
-	echo ${arquivo} > a.out 2> result/stderr.$ext
-	cd ../..
+    
+    [ $DEBUG -eq 1 ] && echo "Exec phase: stdout > result/stdout.$ext and stderr > result/stderr.$ext"
+    
+	cat ${arquivo} | ./a.out > result/stdout.$ext 2> result/stderr.$ext
+
+	cd ${old_dir}
 }
 
-# Main loop: look for new jobs every 1 sec
+# ----------------------------------------------------------------------
+# Main function of the script
+# ----------------------------------------------------------------------
+
+[ $DEBUG -eq 1 ] && echo "Starting the script..."
+    
+# Main loop: look for new jobs every X sec
 while :; do
 
 	sleep 5
 
 	# if there are new jobs
-	if [ `ls JOBS/ | grep -E ^Job. | wc -l` -gt 0 ]
+    njobs=`ls JOBS | grep ^Job 2> /dev/null | wc -l`
+    [ $DEBUG -eq 1 ] && echo "Number of Jobs: $njobs"
+
+	if [ ${njobs} -gt 0 ]
 	then
 		path=`ls JOBS/ | head -1`
+        [ $DEBUG -eq 1 ] && echo "PATH=$path"
+        
 		mv JOBS/$path DOING/
 		compile $path
 		for i in `ls DOING/$path/ | grep -E ^in.*`;
@@ -46,8 +76,13 @@ while :; do
 			execut $i $path
 			differ $i $path
 		done
-		mv DOING/$path DONE/
+
+        [ $DEBUG -eq 1 ] && echo "Moving JOB to DONE folder"
+		rm DOING/$path/a.out
+        mv DOING/$path DONE/
+        
+
 	else
-		echo "Nothing to do..."
+		[ $DEBUG -eq 1 ] && echo "Nothing to be done (Sleeping...)"
 	fi
 done
