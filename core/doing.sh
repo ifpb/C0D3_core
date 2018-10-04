3c8f47b887f49a93ed4a9019706b2d86bf8f8e84
Michael Almeida da Franca Monteiro <michael.cstrc@gmail.com>
2018-10-04 00:03:28 -0300

    [feat22] Expansão do core para C++
      Foram criados dois novos arquivos, sendo eles 'c_assets.sh' e
      'cpp_assets.sh', cujos respectivamente contém as funções de compilação
      e execução dos códigos recebidos juntamente com os Jobs nas linguagens
      c e c++, as funções dos dois arquivos foram importadas para o arquivo
      doing.sh, que agora checa qual é a linguagem na qual foi submetida o
      código do Job e armazena na variável 'current_language', com ela são
      usados 'cases' para verificar a linguagem e executar as funções
      referentes a ela. Os arquivos 'c_assets' e 'cpp_assets' foram criados
      no diretório 'langport' que se encontra no diretório 'core'.
    
      Para fins de organização, foi criado um diretório de nome 'c_samples'
      onde se encontram todos os exemplos de Job que havíamos criado até o
      momento (todos eles com códigos na linguagem C), foi criado também um
      diretório de nome 'cpp_samples' que é onde ficará os exemplos de Job
      com códigos na linguagem C++ e assim serão criados diretórios seguindo
      o mesmo padrão para as outras linguagens à medida que o core for sendo
      expandido.
    
      PS.: O exemplo de Job com código em C++ deve ser refeito pois apenas
      foi copiado o Job com código em C e mudado o nome do arquivo para
      code.cpp assim como o conteúdo do arquivo 'language' no diretório
      'meta' do Job. Necessário também criar outros exemplos de Job como hão
      na linguagem C.

--- core/doing.sh
+++ core/doing.sh
@@ -51,6 +51,7 @@
 # by step of this runnable file are printed to the output environment.
 
 # Global Options
+
 DEBUG=1
 OUTPUT_SIZE_LIMIT=$((1024*1024+1))
 CODE_SIZE_LIMIT=$((50*1024))
@@ -59,6 +60,12 @@ BASE_DIR=""
 DEFAULT_MEMORY_LIMIT=$((50*1024)) # in KB
 STEP_TIME=10
 COLOR_SCHEME=1
+
+# Importing functions related to compiling and execution of different
+# programming languages
+
+source langport/c_assets.sh
+source langport/cpp_assets.sh
 
 # This function finds the base directory for this script and goes to it!
 #
@@ -78,46 +85,6 @@ function goto_base_dir()
 			exit 0
 		fi
 	done
-}
-
-
-# This function compiles the problem code given within the Job directory
-#
-# First it Checks the size of the code file.
-# Then, it finds the code file on the Job directory, generates two files named
-# compile.out and compile.return in the 'result' directory which
-# respectively have the output of the compiling command and the return
-# code of the execution of the same command. The function then return
-# this code too, indicating success or failure in compiling the code.
-function compile()
-{
-	pathc=$1
-	old_dir=$(pwd)
-	cd DOING/$pathc
-
-	# find the code_file on the folder
-	code_to_be_judged=( $(ls | grep *.c) )
-	code_to_be_judged=${code_to_be_judged[0]}
-
-	[ $DEBUG -eq 1 ] && wait_debug "code to be judged=${code_to_be_judged}"
-
-	mkdir result
-
-	# Code size limit
-	code_size=$(ls -l ${code_to_be_judged} | cut -d " " -f 5)
-
-	[ $DEBUG -eq 1 ] && information_debug "Code size=${code_size}"
-
-	if [ ${code_size} -gt ${CODE_SIZE_LIMIT} ]; then
-		cd ${old_dir}
-		return 255
-	fi
-
-	gcc ${code_to_be_judged} &> result/compile.out
-	returner=`echo $?`
-	echo ${returner} > result/compile.return
-	cd ${old_dir}
-	return ${returner}
 }
 
 # This function registers the differences between two outputs
@@ -236,78 +203,6 @@ function general_judge()
 	esac
 
 	cd ${old_dir}
-}
-
-# This function runs the executable file got from the compiling
-#
-# It runs the executable file got from the compiling process of the code
-# file, it creates two files for each test case, both files are created
-# within the 'result' directory in the Job directory. The files are
-# created with the content of the default output from the execution and
-# the error output of the execution.
-#
-# This function also returns the running time of the execution to a
-# file named 'return_time.$ext' in which '$ext' refers to the extension
-# of the test case. An example would be, when the input file is 'in.0',
-# the running time related file will be 'running_time.0'.
-
-function execut()
-{
-	arquivo=$1
-	pathe=$2
-	old_dir=$(pwd)
-
-	ext=`echo ${arquivo} | cut -d. -f2`
-	cd DOING/$pathe
-
-	[ $DEBUG -eq 1 ] && wait_debug "Exec phase: stdout > result/stdout.$ext and stderr > result/stderr.$ext"
-
-	# miliseconds
-	total_time_limit=`cat meta/time_limit`
-	step_time_in_seconds=`echo "scale=2; ${STEP_TIME} / 1000.0" | bc`
-	total_running_time=0
-	memory_limit=`cat meta/memory_limit`
-
-	if [ ${memory_limit} -gt ${DEFAULT_MEMORY_LIMIT} ]
-	then
-		memory_limit=${DEFAULT_MEMORY_LIMIT}
-	fi
-
-	(
-		ulimit -v ${memory_limit}
-		echo ${BASHPID} > exec.pid;
-		{
-			echo "0" > result/retcode.$ext;
-			./a.out < ${arquivo} | head -c ${OUTPUT_SIZE_LIMIT} > result/stdout.$ext;
-			echo ${PIPESTATUS[0]} > result/retcode.$ext;
-		} 2> result/stderr.$ext
-	) 2> /dev/null &
-
-	sleep ${step_time_in_seconds}
-	sub_shell_pid=$(cat exec.pid)
-	rm exec.pid
-
-	while [ "${total_running_time}" -lt "${total_time_limit}" ]
-	do
-		kill -0 ${sub_shell_pid} &> /dev/null
-		[ $? -ne 0 ] && break;
-		sleep ${step_time_in_seconds}
-		total_running_time=$((${total_running_time} + ${step_time}))
-	done
-
-	kill -9 ${sub_shell_pid} &> /dev/null
-	wait ${sub_shell_pid} 2>/dev/null
-
-	execcode=$(cat result/retcode.$ext)
-
-	if [ "${total_running_time}" -ge "${total_time_limit}" ]; then
-		execcode=255
-	fi
-
-	echo ${total_running_time} > result/running_time.$ext
-
-	cd ${old_dir}
-	return ${execcode}
 }
 
 # This function sets the color scheme for the 'error' messages
@@ -479,7 +374,9 @@ while :; do
 	reschkjob=$?
 
 	case ${reschkjob} in
-		0)	[ $DEBUG -eq 1 ] && accept_debug "The format of the Job is OK!"	;;
+		0)	[ $DEBUG -eq 1 ] && accept_debug "The format of the Job is OK!"
+			current_language=`cat JOBS/$path/meta/language`
+			;;
 		1)	[ $DEBUG -eq 1 ] && error_debug "The meta directory doesn't seems to exist..."	;;
 		2)	[ $DEBUG -eq 1 ] && error_debug "One of the files that should be within the meta directory doesn't exist..." ;;
 		3)	[ $DEBUG -eq 1 ] && error_debug "The code file doesn't exist..." ;;
@@ -494,7 +391,7 @@ while :; do
 
 	# Moving the Job to the Working directory
 	mv JOBS/$path DOING/
-	
+
 	# Job Format error
 	if [ ${reschkjob} -ne 0 ]
 	then
@@ -502,11 +399,36 @@ while :; do
 
 		[ $DEBUG -eq 1 ] && wait_debug "Moving JOB to DONE folder"
 		mv DOING/$path DONE/
-		continue		
+		continue
 	fi
-	
-	compile $path
-	retcode=$?
+
+	case $current_language in
+		c)
+			c_compile $path
+			retcode=$?
+		;;
+		c++)
+			cpp_compile $path
+			retcode=$?
+		;;
+		java)
+			java_compile $path
+			retcode=$?
+		;;
+		python2)
+			py2_compile $path
+			retcode=$?
+
+		;;
+		python3)
+			py2_compile $path
+			retcode=$?
+		;;
+		pascal)
+			pas_compile $path
+			retcode=$?
+		;;
+	esac
 
 	# Code size error
 	if [ ${retcode} -eq 255 ]
@@ -529,14 +451,69 @@ while :; do
 	fi
 
 	# The compiling phase was well done
-	for i in `ls DOING/$path/ | grep -E ^in.*`;
-	do
-		execut $i $path
-		execcode=$?
 
-		[ ${execcode} -eq 0 ] && differ $i $path 
-		judge $i $path ${execcode}
-	done
+	case $current_language in
+		c)
+			for i in `ls DOING/$path/ | grep -E ^in.*`;
+			do
+				c_execut $i $path
+				execcode=$?
+
+				[ ${execcode} -eq 0 ] && differ $i $path 
+				judge $i $path ${execcode}
+			done
+		;;
+		c++)
+			for i in `ls DOING/$path/ | grep -E ^in.*`;
+			do
+				cpp_execut $i $path
+				execcode=$?
+
+				[ ${execcode} -eq 0 ] && differ $i $path 
+				judge $i $path ${execcode}
+			done
+		;;
+		java)
+			for i in `ls DOING/$path/ | grep -E ^in.*`;
+			do
+				java_execut $i $path
+				execcode=$?
+
+				[ ${execcode} -eq 0 ] && differ $i $path 
+				judge $i $path ${execcode}
+			done
+		;;
+		python2)
+			for i in `ls DOING/$path/ | grep -E ^in.*`;
+			do
+				py2_execut $i $path
+				execcode=$?
+
+				[ ${execcode} -eq 0 ] && differ $i $path 
+				judge $i $path ${execcode}
+			done
+		;;
+		python3)
+			for i in `ls DOING/$path/ | grep -E ^in.*`;
+			do
+				py3_execut $i $path
+				execcode=$?
+
+				[ ${execcode} -eq 0 ] && differ $i $path 
+				judge $i $path ${execcode}
+			done
+		;;
+		pascal)
+			for i in `ls DOING/$path/ | grep -E ^in.*`;
+			do
+				pas_execut $i $path
+				execcode=$?
+
+				[ ${execcode} -eq 0 ] && differ $i $path 
+				judge $i $path ${execcode}
+			done
+		;;
+	esac
 
 	# General Judging
 	general_judge $path
